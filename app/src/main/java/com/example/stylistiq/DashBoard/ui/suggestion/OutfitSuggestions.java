38fd79c49955bd5e0294158ea2079f6b4a4c2837
Hitesh <hjoshi30042001@gmail.com>
2024-01-22 14:57:06 +0530

    Created schedule outfit layout and changed some layouts.

---
+++ app/src/main/java/com/example/stylistiq/DashBoard/ui/suggestion/OutfitSuggestions.java
@@ -0,0 +1,16 @@
+package com.example.stylistiq.DashBoard.ui.suggestion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.stylistiq.R;
+
+public class OutfitSuggestions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.outfit_suggestions);
+    }
+}
